ae845d7ca34d0afa540c49ecaed227a35210e5b6
hasanmaul <toyohasan@gmail.com>
2017-02-22 11:18:06 +0700

    delete

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -138,68 +138,9 @@ Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, Sony
                                                 </div> 
                                             </li>
                                         </ul>
-                            </li>
-                            <li class="login_box" id="loginContainer">
-                                    <div class="search-box">
-                                        <div id="sb-search" class="sb-search">
-                                            <form>
-                                                <input class="sb-search-input" placeholder="Enter your search term..." type="search" id="search">
-                                                <input class="sb-search-submit" type="submit" value="">
-                                                <span class="sb-icon-search"> </span>
-                                            </form>
-                                        </div>
-                                    </div>
-                                        <!-- search-scripts -->
-                                        <script src="{{ url('/js/classie.js') }}"></script>
-                                        <script src="{{ url('/js/uisearch.js') }}"></script>
-                                            <script>
-                                                new UISearch( document.getElementById( 'sb-search' ) );
-                                            </script>
-                                        <!-- //search-scripts -->
-                            </li>
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-bell"></i><span class="badge blue">3</span></a>
-                                    <ul class="dropdown-menu">
-                                        <li>
-                                            <div class="notification_header">
-                                                <h3>You have 3 new notification</h3>
-                                            </div>
-                                        </li>
-                                        <li><a href="#">
-                                            <div class="user_img"><img src="images/1.png" alt=""></div>
-                                           <div class="notification_desc">
-                                            <p>Lorem ipsum dolor sit amet</p>
-                                            <p><span>1 hour ago</span></p>
-                                            </div>
-                                          <div class="clearfix"></div>  
-                                         </a></li>
-                                         <li class="odd"><a href="#">
-                                            <div class="user_img"><img src="images/1.png" alt=""></div>
-                                           <div class="notification_desc">
-                                            <p>Lorem ipsum dolor sit amet </p>
-                                            <p><span>1 hour ago</span></p>
-                                            </div>
-                                           <div class="clearfix"></div> 
-                                         </a></li>
-                                         <li><a href="#">
-                                            <div class="user_img"><img src="images/1.png" alt=""></div>
-                                           <div class="notification_desc">
-                                            <p>Lorem ipsum dolor sit amet </p>
-                                            <p><span>1 hour ago</span></p>
-                                            </div>
-                                           <div class="clearfix"></div> 
-                                         </a></li>
-                                         <li>
-                                            <div class="notification_bottom">
-                                                <a href="#">See all notification</a>
-                                            </div> 
-                                        </li>
-                                    </ul>
-                            </li>   
-                            <li class="dropdown">
-                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" aria-expanded="false"><i class="fa fa-tasks"></i><span class="badge blue1">22</span></a>
-                                    <ul class="dropdown-menu">
-                                        </ul>                                             
+                            </li>                           
+                            
+                                                        
                             <div class="clearfix"></div>    
                         </ul>
                     </div>
